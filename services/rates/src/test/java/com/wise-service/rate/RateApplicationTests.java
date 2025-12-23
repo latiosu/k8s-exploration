dba3536b123b6cf32a61aebeb6915ae440c7a462
Ryukom <ryukom@MacBook-Pro.local>
2025-12-23 13:01:59 +1100

    feat: persist the intial wise rate microservice

---
+++ services/rates/src/test/java/com/wise-service/rate/RateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
